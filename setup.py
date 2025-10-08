460be075e3a32e46b8730f18560e0a68ecad822e
Anish Bonda <70102113+AnishBonda20@users.noreply.github.com>
2025-10-08 12:23:14 +0530

    py files only

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='rootkit_scanner',
+    version='0.1',
+    py_modules=['rootkitscanner', 'rootkit_detector', 'memory_capture'],
+    install_requires=[],
+)
